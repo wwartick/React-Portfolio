ddfdf82e583b84c4445019cd5824270ac1767ee5
wwartick <willwartick@gmail.com>
2022-12-17 15:00:20 -0800

    updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,19 +13,18 @@ function About() {
             <p style={{ color: 'silver', textAlign: 'left', fontSize: '20px', letterSpacing: '2px', lineHeight: '4' }}>
              I am a <strong>Full-stack Web Developer</strong> that has recently graduated from <strong>UC Davis' full stack web development boot camp</strong>.
             </p>
-            <p style={{ color: 'silver', textAlign: 'left', fontSize: '20px', letterSpacing: '2px', lineHeight: '2' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-              qui officia deserunt mollit anim id est laborum.
+            <p style={{ color: 'silver', textAlign: 'left', fontSize: '20px', letterSpacing: '2px', lineHeight: '1.5' }}>
+              I am currently searching for employment as a <strong>web developer</strong> with my newly learned skills. In the projects tab, you can find the group projects
+              that I built with my classmates. Our course gave us no direction on these, other than a requirement on the technologies used. <strong>'Weeb Army'</strong> was the first project 
+              given, and in less than a month, my skills had advanced to creating <strong>'Movie-App'</strong>. For our final project, my group and I used all of the knowledge we had learned, and built
+              a full stack e-commerce dummy site: <strong>'Music Market'</strong>
             </p>
-            <p style={{ color: 'silver', textAlign: 'left', fontSize: '20px', letterSpacing: '2px', lineHeight: '2' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-              qui officia deserunt mollit anim id est laborum.
+            <p style={{ color: 'silver', textAlign: 'left', fontSize: '20px', letterSpacing: '2px', lineHeight: '1.5' }}>
+              I have spent the last 10 years in and out of college at San Francisco State University and various community colleges to further my education.
+              In the meantime, I have been serving at local restaurants for employment and helping my friends and family with computer issues in my free time. I'm passionate in my 
+              journey to continue learning, as this recent boot camp opened my eyes to how in-depth coding can be, and the infinite capabilities with these skills. In my free time,
+              I play sports with my friends like basketball or football. If I'm not playing sports, I'm watching my favorite teams: Green Bay Packers, St. Louis Cardinals, Golden State Warriors, and Manchester City.
+              Other than that, I play a lot of video games and spend time with my dogs. If you read all this text I thank you, please reach out with any questions you might have!
             </p>
           </div>
         </div>

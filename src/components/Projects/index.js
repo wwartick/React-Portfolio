a9fefa754dbe2820c53785c8027759e473aee3ed
wwartick <willwartick@gmail.com>
2022-12-17 12:30:47 -0800

    projects page

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,27 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react'
+import './style.css';
+
+import img0 from '../../assets/0.png';
+import img1 from '../../assets/1.png'
+import img2 from '../../assets/2.png';
+import img3 from '../../assets/3.png';
+
+const images = {
+  '0': img0,
+  '1': img1,
+  '2': img2,
+  '3': img3,
+};
 
 function Projects() {
+  const [projects, setProjects] = useState([
+    {
+      title: 'Music Market',
+      deployedURL: 'https://g02-music-market.herokuapp.com/',
+      githubURL: 'https://github.com/jacobmcanepa/music-market',
+      img: '0',
+      technologies: 'MERN stack, Cloudinary React SDK, GraphQL, Apollo, Stripe, CSS',
+      opacity: 0.5
+    },
+    {
+      title: 'Movie App',
+      deployedURL: 'https://jc-movie-app.herokuapp.com/',
+      githubURL: 'https://github.com/jacobmcanepa/movie-app',
+      img: '1',
+      technologies: 'HTML(Handlebars), CSS, JS, Node.js, SQL, Sequelize, Express, OpenAI',
+      opacity: 0.5
+    },
+    {
+      title: 'Weeb Army',
+      deployedURL: 'https://desguerra.github.io/weeb-army/',
+      githubURL: 'https://github.com/desguerra/weeb-army',
+      img: '2',
+      technologies: 'HTML, CSS, JavaScript, jQuery',
+      opacity: 0.5
+    },
+    {
+      title: 'Weather Dashboard',
+      deployedURL: 'https://wwartick.github.io/Weather-Dashboard/',
+      githubURL: 'https://github.com/wwartick/Weather-Dashboard',
+      img: '3',
+      technologies: 'HTML, CSS, JavaScript, jQuery',
+      opacity: 0.5
+    },
+  ]);
+
+  const handleMouseEnter = (index) => {
+    projects[index].opacity = 1;
+    setProjects([...projects]);
+  };
+
+  const handleMouseLeave = (index) => {
+    projects[index].opacity = 0.5;
+    setProjects([...projects]);
+  };
+
   return (
-    <section className='container-fluid'>
-      <div className='mx-5 py-2'>
-      <h2 style={{ color: '#F5B041' }}>My Projects</h2>
-        <div className='p-5'>
-          <div>
-          <p style={{ color: 'silver' }}>
-              Hey there, I'm <strong>Will</strong>.
-            </p>
-            <p style={{ color: 'silver' }}>
-              I am a <strong>Full-stack Web Developer</strong> that has recently graduated from <strong>UC Davis' full stack web development boot camp</strong>.
-            </p>
-            <p style={{ color: 'silver' }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
-              qui officia deserunt mollit anim id est laborum.
-            </p>
+    <section>
+      <h4>Hover over a project to see the technologies used</h4>
+      <div className="card-container">
+        {projects.map((project, index) => (
+          <div
+            className="card"
+            style={{opacity: project.opacity}}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+          >
+          <img src={images[project.img]} alt={project.title} title={project.technologies} style={{height: '350px', width: '70%'}} className ='tooltip'/>
+            <div>
+            <h2>{project.title}</h2>
+            <a className="silver-link" href={project.deployedURL}>Deployed  Website</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <a className="silver-link" href={project.githubURL}>Github Repo</a>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
       <div>
       <h2 style={{fontSize: '30px', paddingTop: '40px', color: '#F5B041' }}>
       <a href={resume} download style={{ color: '#F5B041' }}>
-          Click here to download my resume!
+      →→ Click here to download my resume! ←←
           </a>
         </h2>
         <div
@@ -22,7 +22,7 @@ function Resume() {
             }}
           >
             <h3 style={{fontSize: '30px', paddingTop: '10px', color: '#F5B041', textDecoration: 'underline' }}>Front-end technologies</h3>
-            <ul style={{ listStyleType: 'none', color: 'silver', fontSize: '15px', letterSpacing: '2px', lineHeight: '2.5' }}> 
+            <ul style={{ listStyleType: 'none', color: 'silver', fontSize: '18px', letterSpacing: '2px', lineHeight: '2.5' }}> 
               <li>React</li>
               <li>HTML</li>
               <li>CSS</li>
@@ -33,7 +33,7 @@ function Resume() {
             </ul>
           </div>
 
-          <div style={{ listStyleType: 'none', color: 'silver', fontSize: '15px', letterSpacing: '2px', lineHeight: '1.5' }}> 
+          <div style={{ listStyleType: 'none', color: 'silver', fontSize: '22px', letterSpacing: '2px', lineHeight: '1.5' }}> 
           <h4 style = {{ letterSpacing: '1px'}}> Or reach out to me using the links below.</h4>
           <p><a href='https://www.linkedin.com/in/wwartick/' download style={{ color: 'silver' }}>LinkedIn Profile</a></p>
           <p><a href='https://github.com/wwartick' download style={{ color: 'silver' }}>Github Profile</a></p>
@@ -46,7 +46,7 @@ function Resume() {
             }}
           >
             <h3 style={{fontSize: '30px', paddingTop: '10px', color: '#F5B041', textDecoration: 'underline' }}>Back-end technologies</h3>
-            <ul style={{ listStyleType: 'none', color: 'silver', fontSize: '15px', letterSpacing: '2px', lineHeight: '2.5' }}> 
+            <ul style={{ listStyleType: 'none', color: 'silver', fontSize: '18px', letterSpacing: '2px', lineHeight: '2.5' }}> 
               <li>APIs</li>
               <li>Node JS</li>
               <li>Experss.js</li>
